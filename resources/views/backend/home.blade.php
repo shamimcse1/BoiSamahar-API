17c9cdef5eaf0316462adf55abeee491164fae8a
shamimcse1 <mdshamimh529@gmail.com>
2022-10-25 22:03:08 +0600

    remove header dashboard

--- resources/views/backend/home.blade.php
+++ resources/views/backend/home.blade.php
@@ -6,7 +6,7 @@
 
           <x-slot name='breadCrumb'>
               <x-backend.layouts.elements.breadcrumb>
-                  <x-slot name="pageHeader"> Dashboard </x-slot>
+                  <x-slot name="pageHeader">  </x-slot>
                   <li class="breadcrumb-item active">Dashboard</li>
               </x-backend.layouts.elements.breadcrumb>
           </x-slot>
